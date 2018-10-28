036ea3dbbec995adf9251dda077e1e230850baba
Black <freedomandjustit@gmail.com>
2018-10-28 16:03:49 +0100

    Change freeze on env level

--- RemoteDataBases/wsgi.py
+++ RemoteDataBases/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for InvoiceService project.
+WSGI config for RemoteDataBases project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InvoiceService.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RemoteDataBases.settings')
 
 application = get_wsgi_application()
