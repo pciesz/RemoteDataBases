abf80e9bf6851e612ffe1f27586924a1bd369baf
pciesz <cieszko.stud@gmail.com>
2018-12-05 08:23:34 +0100

    login fix

--- user/forms.py
+++ user/forms.py
@@ -7,7 +7,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
 
 
 class UserLoginForm(forms.Form):
